c3e697b26daa993890bab585ee043ca5843bde64
josh <jhell85@gmail.com>
2020-02-14 10:49:42 -0800

    added button to call functions

--- src/main.js
+++ src/main.js
@@ -4,3 +4,29 @@ import './styles.css';
 import $ from "jquery";
 import { Doctor } from './better-doctor';
 
+
+
+
+$(document).ready(function() {
+
+  $('#b').click(function() {
+    const docName = "Smith";
+    (async () => {
+      console.log("button clicked");
+      
+      let doctor = new Doctor();
+      const response = await doctor.getDoctor(docName);
+      console.log(response);
+      getElements(response);
+
+    })();
+
+    function getElements(response){
+    }
+
+
+
+
+
+  });
+});
