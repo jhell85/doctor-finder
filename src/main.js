3121f968969f60ae0fc8ed1e2911e7c58b3b95ee
josh <jhell85@gmail.com>
2020-02-14 12:43:58 -0800

    list of doctors displays on page instead of console

--- src/main.js
+++ src/main.js
@@ -5,32 +5,28 @@ import $ from "jquery";
 import { Doctor } from './better-doctor';
 
 
+function showDoctors(docArray) {
+  for (let i=0; i<docArray.length; i++) {
+    // console.log(`First name : ${doctor.profile.first_name} Last name:${doctor.profile.last_name}`);
+    $("#docList").append(`<li class="docList" value="${docArray[i]}">${docArray[i].profile.first_name} ${docArray[i].profile.last_name}</li>`);
+  }
+}
 
 
 $(document).ready(function() {
-
-  $('#b').click(function() {
-    const docName = "Smith";
+  $('#search').click(function() {
+    const docName = $("#name").val();
     (async () => {
       let doctor = new Doctor();
       const response = await doctor.getDoctor(docName);
       getElements(response.data);
-
     })();
 
     function getElements(response){
-      // console.log(response);
-      response.forEach(doctor => {
-        console.log(`First name : ${doctor.profile.first_name} Last name:${doctor.profile.last_name}`);
-      });
-   
-      
-
+      if (response.length === 0) {
+        $("#output").text("Sorry no Doctors found.");
+      } else 
+        showDoctors(response);
     }
-
-
-
-
-
   });
 });
