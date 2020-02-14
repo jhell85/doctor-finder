9c7efb191f159d0e00dfa683049a6fae932e0888
josh <jhell85@gmail.com>
2020-02-14 10:53:30 -0800

    fixed url for api call

--- src/better-doctor.js
+++ src/better-doctor.js
@@ -1,7 +1,10 @@
 export class Doctor {
+
   async getDoctor(doctorType){
+    console.log("getDoctor()");
+    
     try{
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorType}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
       let jsonifiedResopnse;
       if (response.status === 200 && response.ok) {
         jsonifiedResopnse = await response.json();
