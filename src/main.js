0fb5cc5cb2dbef7d6f5f75fd6e2afbe35259d843
josh <jhell85@gmail.com>
2020-02-14 15:00:34 -0800

    added jquery toggle to show and hide stuff on page

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
@@ -7,18 +8,31 @@ import { Doctor } from './better-doctor';
 
 function showDoctors(docArray) {
   for (let i=0; i<docArray.length; i++) {
-    // console.log(`First name : ${doctor.profile.first_name} Last name:${doctor.profile.last_name}`);
-    $("#docList").append(`<li class="docList" value="${docArray[i]}">${docArray[i].profile.first_name} ${docArray[i].profile.last_name}</li>`);
+    
+    $("#docList").append(`<tr class="table-info docList" value="${docArray[i].uid}"><td>${i + 1}</td><td scope="row">${docArray[i].profile.first_name}</td><td>${docArray[i].profile.last_name}</td><td><button value="${docArray[i].uid}" class="btn btn-primary">Details</button></td></tr>`);
   }
 }
 
+function showDocInfo(ID, data){
+  data.forEach(doctor => {
+    if (doctor.uid === ID){
+      
+      $(".table").fadeOut();
+      console.log(doctor.profile.last_name);
+      
+    }
+    
+  });
+  console.log(ID);
+}
 
 $(document).ready(function() {
+  let response;
   $('#search').click(function() {
     const docName = $("#name").val();
     (async () => {
       let doctor = new Doctor();
-      const response = await doctor.getDoctor(docName);
+      response = await doctor.getDoctor(docName);
       getElements(response.data);
     })();
 
@@ -26,7 +40,13 @@ $(document).ready(function() {
       if (response.length === 0) {
         $("#output").text("Sorry no Doctors found.");
       } else 
-        showDoctors(response);
+        $(".table").show(1000);
+      showDoctors(response);
     }
+    $(".table").on("click", "button", function() {
+      let doctorID = $(this).attr("value");
+      showDocInfo(doctorID, response.data);
+    });
+
   });
 });
