1bdb778a21b245bfb4d4ad94f48810f907352d0f
josh <jhell85@gmail.com>
2020-02-14 12:48:44 -0800

    disabled eslint yelling about error in api call

--- src/better-doctor.js
+++ src/better-doctor.js
@@ -3,6 +3,7 @@ export class Doctor {
   async getDoctor(doctorType){
     
     try{
+      // eslint-disable-next-line no-undef
       let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorType}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
       let jsonifiedResopnse;
       if (response.status === 200 && response.ok) {
