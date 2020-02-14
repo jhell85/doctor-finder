c89e7648f36aeaf432e35bb481ffba07f75a52b5
josh <jhell85@gmail.com>
2020-02-14 09:46:25 -0800

    updated structure of files

---
+++ src/better-doctor.js
@@ -0,0 +1,16 @@
+export class Doctor {
+  async getDoctor(doctorType){
+    try{
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/practices?location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`)
+      let jsonifiedResopnse;
+      if (response.status === 200 && response.ok) {
+        jsonifiedResopnse = await response.json();
+      } else {
+        jsonifiedResopnse = false;
+      }
+      return jsonifiedResopnse;
+    } catch(error) {
+      console.warn("Error caught >>>");
+    }
+  }
+}
