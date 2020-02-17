eb0e560e51f87950e2582f1f17d9776d6a2f4b0e
josh <jhell85@gmail.com>
2020-02-16 19:28:13 -0800

    upadated api call functions

--- src/better-doctor.js
+++ src/better-doctor.js
@@ -1,13 +1,22 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 export class Doctor {
 
-  async getDoctor(doctorType){
-    
+  async getDoctor(doctorName, doctorSpecialty){
+    // eslint-disable-next-line no-undef
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&specialty_uid=${doctorSpecialty}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`;
+    const url2 = `https://api.betterdoctor.com/2016-03-01/doctors?name=josh&specialty_uid=cardiologist&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`;
     try{
+      
       // eslint-disable-next-line no-undef
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorType}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(url);
+
+
       let jsonifiedResopnse;
       if (response.status === 200 && response.ok) {
         jsonifiedResopnse = await response.json();
+        console.log(jsonifiedResopnse);
       } else {
         jsonifiedResopnse = false;
       }
@@ -16,5 +25,13 @@ export class Doctor {
       // eslint-disable-next-line no-console
       console.warn("Error caught >>>");
     }
+  }
+
+  checkInput(input){
+    if (input == "" || input ==" "){
+      return "";
+    }
+    else 
+      return `specialty_uid=${input}`;
   }
 }
