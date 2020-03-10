038e07bd7a9cf8b4cf14ab44a5b4579980c706aa
josh <jhell85@gmail.com>
2020-03-09 17:49:32 -0700

    added google maps api async call to get geo location

--- src/better-doctor.js
+++ src/better-doctor.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 export class Doctor {
-
-  async getDoctor(doctorName, doctorSpecialty){
+  async getDoctor(doctorName, doctorSpecialty) {
     // eslint-disable-next-line no-undef
     const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&specialty_uid=${doctorSpecialty}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`;
-    try{
+    try {
       let response = await fetch(url);
       let jsonifiedResopnse;
       if (response.status === 200 && response.ok) {
@@ -15,25 +14,34 @@ export class Doctor {
         jsonifiedResopnse = false;
       }
       return jsonifiedResopnse;
-    } catch(error) {
+    } catch (error) {
       // eslint-disable-next-line no-console
       console.warn("Error caught >>>");
     }
   }
-  async getSpecialties(){
-  // eslint-disable-next-line no-undef
-    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/specialties?user_key=${process.env.API_KEY}`);
-
-
-    let jsonifiedResopnse;
-    if (response.status === 200 && response.ok) {
-      jsonifiedResopnse = await response.json();
-    } else {
-      jsonifiedResopnse = false;
+  async getSpecialties() {
+    // eslint-disable-next-line no-undef
+    try {
+      let response = await fetch(
+        `https://api.betterdoctor.com/2016-03-01/specialties?user_key=${process.env.API_KEY}`
+      );
+      let jsonifiedResopnse;
+      if (response.status === 200 && response.ok) {
+        jsonifiedResopnse = await response.json();
+      } else {
+        jsonifiedResopnse = false;
+      }
+      return jsonifiedResopnse;
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn("Error caught >>>");
     }
-    return jsonifiedResopnse;
-  } catch(error) {
-    // eslint-disable-next-line no-console
-    console.warn("Error caught >>>");
+  }
+  async getLocation() {
+    let response = await fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyBEKom_qnRKCiDRSQ8yM8YY0wemlIcSqHQ"
+    );
+    return response;
+
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,30 +1,45 @@
 /* eslint-disable no-console */
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles.css";
 import $ from "jquery";
-import { Doctor } from './better-doctor';
-
+import { Doctor } from "./better-doctor";
 
 function showDoctors(docArray) {
-  for (let i=0; i<docArray.length; i++) {
-    
-    $("#docList").append(`<tr class="table-info docList" value="${docArray[i].uid}"><td>${i + 1}</td><td scope="row">${docArray[i].profile.first_name}</td><td>${docArray[i].profile.last_name}</td><td><button value="${docArray[i].uid}" class="btn btn-primary">Details</button></td></tr>`);
+  for (let i = 0; i < docArray.length; i++) {
+    $("#docList").append(
+      `<tr class="table-info docList" value="${docArray[i].uid}"><td>${i +
+        1}</td><td scope="row">${docArray[i].profile.first_name}</td><td>${
+        docArray[i].profile.last_name
+      }</td><td><button value="${
+        docArray[i].uid
+      }" class="btn btn-primary">Details</button></td></tr>`
+    );
   }
 }
 
-function doctorSpecialties(specialtiesArray){
-  let htmlSpecialties = ""; 
+function doctorSpecialties(specialtiesArray) {
+  let htmlSpecialties = "";
   specialtiesArray.forEach(specialty => {
     htmlSpecialties += `<li>${specialty.name}</li>`;
   });
   return htmlSpecialties;
 }
 
-function fillPracticesTable(practices){
-  $("#practicesTable").html(`<thead><tr class="table-primary"><th scope="col">address</th><th scope="col">phone</th><th scope="col">website</th></tr></thead>`);
+function fillPracticesTable(practices) {
+  $("#practicesTable").html(
+    `<thead><tr class="table-primary"><th scope="col">address</th><th scope="col">phone</th><th scope="col">website</th></tr></thead>`
+  );
   practices.forEach(practice => {
-    $("#practicesTable").append(`<tr class="table-info"><td><b>${practice.name}</b><br>${practice.visit_address.street}<br>${practice.visit_address.city} ${practice.visit_address.state} ${practice.visit_address.zip}</td><td>${formatPhoneNumber(practice.phones)}</td><td><a href="${practice.website}">website</a></td></tr>`); 
+    $("#practicesTable").append(
+      `<tr class="table-info"><td><b>${practice.name}</b><br>${
+        practice.visit_address.street
+      }<br>${practice.visit_address.city} ${practice.visit_address.state} ${
+        practice.visit_address.zip
+      }</td><td>${formatPhoneNumber(practice.phones)}</td><td><a href="${
+        practice.website
+      }">website</a></td></tr>`
+    );
   });
 }
 
@@ -32,11 +47,19 @@ function formatPhoneNumber(phoneNumbers) {
   let htmlToReturn = "";
   phoneNumbers.forEach(pNumber => {
     let pNumberDirty = pNumber.number;
-    let cleaned = ('' + pNumberDirty).replace(/\D/g, '');
+    let cleaned = ("" + pNumberDirty).replace(/\D/g, "");
     let match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
     if (match) {
-      let intlCode = (match[1] ? '+1 ' : '');
-      let pNumberClean = [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+      let intlCode = match[1] ? "+1 " : "";
+      let pNumberClean = [
+        intlCode,
+        "(",
+        match[2],
+        ") ",
+        match[3],
+        "-",
+        match[4]
+      ].join("");
       htmlToReturn += `${pNumber.type} : <br><i>${pNumberClean}</i><br>`;
     }
     return null;
@@ -44,10 +67,14 @@ function formatPhoneNumber(phoneNumbers) {
   return htmlToReturn;
 }
 
-function showDocInfo(ID, data){
+function showDocInfo(ID, data) {
   data.forEach(doctor => {
-    if (doctor.uid === ID){
-      let profileContainer = `<h4>${doctor.profile.first_name} ${doctor.profile.last_name}</h4><div><h5>Specialties:<h5></div><ul class="specialties">${doctorSpecialties(doctor.specialties)}</ul>`;
+    if (doctor.uid === ID) {
+      let profileContainer = `<h4>${doctor.profile.first_name} ${
+        doctor.profile.last_name
+      }</h4><div><h5>Specialties:<h5></div><ul class="specialties">${doctorSpecialties(
+        doctor.specialties
+      )}</ul>`;
       $(".table").fadeOut();
       $(".docInfo").show(1000);
       $(".docProfile").html(profileContainer);
@@ -65,9 +92,11 @@ $(document).ready(function() {
     populateSpecialties(response.data);
   })();
 
-  function populateSpecialties(response){
-    if (!response){
-      $(".body").html('<img src="http://www.findmysoft.com/img/news/inside/Error-401_1460548854.jpg"/>');
+  function populateSpecialties(response) {
+    if (!response) {
+      $(".body").html(
+        '<img src="http://www.findmysoft.com/img/news/inside/Error-401_1460548854.jpg"/>'
+      );
     }
     let specialtiesArray = response;
     specialtiesArray.forEach(specialty => {
@@ -76,22 +105,22 @@ $(document).ready(function() {
     });
   }
 
-  $('#search').click(function() {
+  $("#search").click(function() {
     const docName = $("#name").val();
     const specialty = $("#specialty").val();
     $(".docInfo").fadeOut();
     (async () => {
       response = await doctor.getDoctor(docName, specialty);
-      
+
       getElements(response.data);
     })();
 
-    function getElements(response){
+    function getElements(response) {
       if (response.length === 0) {
         $(".table").hide();
         $("#no_results").show();
         $("#no_results").text("Sorry no Doctors found.");
-      } else{ 
+      } else {
         $("#no_results").hide();
         $(".docList").remove();
         $(".table").show(1000);
@@ -102,6 +131,7 @@ $(document).ready(function() {
       let doctorID = $(this).attr("value");
       showDocInfo(doctorID, response.data);
     });
-
   });
+
+  console.log(doctor.getLocation());
 });
